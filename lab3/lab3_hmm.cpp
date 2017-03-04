50125a6bc92c52d4bbe8e82f92156bd18fff69c0
Sascha Wombacher <sascha.wombacher@gmx.de>
2017-03-04 17:56:03 +0100

    removed glm dependency

--- lab3/lab3_hmm.cpp
+++ lab3/lab3_hmm.cpp
@@ -1,29 +1,52 @@
-#include <glm/mat3x3.hpp>
-#include <iomanip>
 #include <iostream>
+#include <iomanip>
 #include <vector>
+#include <array>
+
+struct Vec3 {
+    union {
+        struct {
+            float x;
+            float y;
+            float z;
+        };
+        float data[3];
+    };
+    constexpr Vec3(float X, float Y, float Z) : x(X), y(Y), z(Z) {}
+    float& operator[](size_t idx) { return data[idx]; }
+    Vec3& operator+=(const Vec3& rhs) {
+        this->x += rhs.x;
+        this->y += rhs.y;
+        this->z += rhs.z;
+        return *this;
+    }
+    Vec3& operator*=(float value) {
+        this->x *= value;
+        this->y *= value;
+        this->z *= value;
+        return *this;
+    }
+};
+typedef std::array<std::array<float, 3>, 3> Mat3x3;
 
 enum CHANGE { NO_CHANGE, GOAL_ENEMY, GOAL_DARMSTADT };
 enum NOISE { OLE, TOOR, OHHH };
-static std::vector<NOISE> noises = {
-    OLE, OLE, OHHH, OLE, TOOR
-};
+static std::vector<NOISE> noises = {OLE, OLE, OHHH, OLE, TOOR};
 static size_t current_noise_idx = 0;
 
-glm::vec3 hiddenMarcovModel(const glm::mat3x3& trans, const glm::mat3x3& sensor,
-                    glm::vec3 probabilities = {1.f / 3.f, 1.f / 3.f, 1.f / 3.f})
-{
-    glm::vec3 newProb(0, 0, 0);
-    for (int i = 0; i < 3; ++i) {
+Vec3 hiddenMarcovModel(const Mat3x3& trans, const Mat3x3& sensor,
+                       Vec3 probabilities = {1.f / 3.f, 1.f / 3.f, 1.f / 3.f}) {
+    Vec3 newProb(0, 0, 0);
+    for (size_t i = 0; i < 3; ++i) {
         std::cout << "Calculation " << i << ":\n(";
-        for (int iter = 0; iter < 3; ++iter) {
+        for (size_t iter = 0; iter < 3; ++iter) {
             newProb[i] += probabilities[iter] * trans[iter][i];
             std::cout << probabilities[iter] << " * " << trans[iter][i]
                       << " + ";
         }
         newProb[i] *= sensor[noises[current_noise_idx]][i];
-        std::cout << "\b\b\b) * "
-                  << sensor[noises[current_noise_idx]][i] << '\n'
+        std::cout << "\b\b\b) * " << sensor[noises[current_noise_idx]][i]
+                  << '\n'
                   << std::endl;
     }
     newProb *= 1.f / (newProb.x + newProb.y + newProb.z);
@@ -37,7 +60,7 @@ glm::vec3 hiddenMarcovModel(const glm::mat3x3& trans, const glm::mat3x3& sensor,
 }
 
 int main() {
-    glm::mat3x3 sensor;
+    Mat3x3 sensor;
     sensor[NOISE::OLE][CHANGE::NO_CHANGE] = 0.8f;
     sensor[NOISE::OLE][CHANGE::GOAL_ENEMY] = 0.1f;
     sensor[NOISE::OLE][CHANGE::GOAL_DARMSTADT] = 0.1f;
@@ -48,7 +71,7 @@ int main() {
     sensor[NOISE::TOOR][CHANGE::GOAL_ENEMY] = 0.2f;
     sensor[NOISE::TOOR][CHANGE::GOAL_DARMSTADT] = 0.8f;
 
-    glm::mat3x3 trans;
+    Mat3x3 trans;
     trans[CHANGE::NO_CHANGE][CHANGE::NO_CHANGE] = 0.6f;
     trans[CHANGE::NO_CHANGE][CHANGE::GOAL_ENEMY] = 0.2f;
     trans[CHANGE::NO_CHANGE][CHANGE::GOAL_DARMSTADT] = 0.2f;
@@ -60,9 +83,10 @@ int main() {
     trans[CHANGE::GOAL_DARMSTADT][CHANGE::GOAL_DARMSTADT] = 0.2f;
 
     std::cout << std::setprecision(3);
-    glm::vec3 vec = hiddenMarcovModel(trans, sensor);
+    Vec3 vec = hiddenMarcovModel(trans, sensor);
     std::cout << "Probability no change: " << vec[CHANGE::NO_CHANGE] << '\n'
-              << "Probability goal darm: " << vec[CHANGE::GOAL_DARMSTADT] << '\n'
+              << "Probability goal darm: " << vec[CHANGE::GOAL_DARMSTADT]
+              << '\n'
               << "Probability goal enem: " << vec[CHANGE::GOAL_ENEMY] << '\n'
               << std::endl;
     return 0;
